0ea3c2ddc3c968ce7272c9b50d6c25a45f146b79
ThatOnePersonHere <JJSonsteng@gmail.com>
2025-04-10 14:04:42 -0700

    commiting first completed bootdotdev project: Bookbot

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("greetings boots")
+def get_book_info(location):
+	from stats import get_num_words
+	from stats import get_character_count
+	book_location = location
+	print(f'Analyzing book found at {location}...')
+	get_num_words(book_location)
+	get_character_count(book_location)
+	print("============= END ===============")
+	
+def main():
+	import sys
+	try:
+		book = sys.argv[1]
+		program_info = "============ BOOKBOT ============"
+		print(program_info)
+		get_book_info(book)
+	except Exception as e:
+		print('Usage: python3 main.py <path_to_book>')
+		sys.exit(1)
+
+main()
---
+++ stats.py
@@ -0,0 +1,30 @@
+def get_num_words(location):
+	selected_book = location
+	print("----------- Word Count ----------")
+	with open(selected_book) as book:
+		book_contents = book.read()
+		book_contents_word_count = len(book_contents.split())
+		print(f'Found {book_contents_word_count} total words')
+
+def print_out_dict(dictonary):
+	for key in dictonary:
+		if key.isalpha():
+			print(f'{key}: {dictonary[key]}')
+
+def get_character_count(location):
+	selected_book = location
+	character_count = {}
+	print('--------- Character Count -------')
+	with open(selected_book) as book:
+		book_contents = book.read()
+		book_lower = book_contents.lower()
+	book_words = book_lower.split()
+	for word in book_words:
+		for letter in word:
+			if letter in character_count:
+				character_count[letter] += 1
+			else:
+				character_count.update({letter : 1})
+	character_count = dict(sorted(character_count.items(), key=lambda item: item[1], reverse=True))
+	#character_count.sort(reverse=True, key="num")	
+	print_out_dict(character_count)
